f0c701b940dab966b30f58390fd9c760086a07d6
Gaurang-dobariya <gaurangdobariya870@gmail.com>
2024-06-28 19:59:23 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./components/pages/Home";
+import Product from "./components/pages/Product";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import ProductCard from "./components/pages/ProductCard";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/:id" element={<ProductCard />} />
+            </Routes>
+
+            <Footer />
+        </>
+    )
+
 }
 
-export default App;
+export default App
